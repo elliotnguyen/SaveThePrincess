c3f3f7d102bafd1709fcd45512530f072df0f580
elliotnguyen <nguyentienthanhgq@gmail.com>
2023-08-14 20:41:29 +0700

    add character selection

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,14 +6,33 @@ public class CameraController : MonoBehaviour
 {
     private Transform player;
     private Vector3 tempPos;
+
+    public float ZoomChange;
+    public float SmoothChange;
+    public float MinSize, MaxSize;
+
+    private Camera cam;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        cam = gameObject.GetComponent<Camera>();
         player = GameObject.FindWithTag("player").transform;
     }
 
+    private void Update()
+    {
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            cam.orthographicSize -= ZoomChange * Time.deltaTime * SmoothChange;
+        } else if (Input.mouseScrollDelta.y < 0)
+        {
+            cam.orthographicSize += ZoomChange * Time.deltaTime * SmoothChange;
+        }
+
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, MinSize, MaxSize);
+    }
     // Update is called once per frame
-    void LateUpdate()
+    private void LateUpdate()
     {
         tempPos = transform.position;
         tempPos.x = player.position.x;
--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -3,14 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField] Sprite[] characterSprite;
+    public SpriteRenderer artWorkSprite;
+
+    private int selectedOption = 0;
+
+    private void Start()
+    {
+        UpdateCharacter(selectedOption);
+    }
+
+    public void NextOption()
+    {
+        selectedOption++;
+
+        if (selectedOption >= characterSprite.Length)
+        {
+            selectedOption = 0;
+        }
+
+        UpdateCharacter(selectedOption);
+    }
+
+    public void BackOption()
+    {
+        selectedOption--;
+
+        if (selectedOption < 0)
+        {
+            selectedOption = characterSprite.Length - 1;
+        }
+
+        UpdateCharacter(selectedOption);
+    }
+
+    private void UpdateCharacter(int selectedOption)
+    {
+        artWorkSprite.sprite = characterSprite[selectedOption];
+    }
+
     public void PlayGame()
     {
-        int selectedCharacter = int.Parse(EventSystem.current.currentSelectedGameObject.name);
+        //int selectedCharacter = int.Parse(EventSystem.current.currentSelectedGameObject.name);
 
-        CharacterSelection.instance.CharIndex = selectedCharacter;
+        //CharacterSelection.instance.CharIndex = selectedCharacter;
+        CharacterSelection.instance.CharIndex = selectedOption;
 
         SceneManager.LoadScene("GamePlay");
 
